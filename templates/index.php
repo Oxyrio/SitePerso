14537541aa77501a30667fe97ecf94c04a4d0f11
Oxyrio <axelandayme@hotmail.fr>
2016-10-24 15:25:01 +0200

    Ajout des div

--- templates/index.php
+++ templates/index.php
@@ -4,33 +4,43 @@ include('header.php');
 
 ?>
 
-<div class="container first">
-    <div class="row">
-        <h1>Section d'entrée</h1>
+<div id="first">
+    <div class="container">
+        <div class="row">
+            <h1>Section d'entrée</h1>
+        </div>
     </div>
 </div>
 
-<div class="container second">
-    <div class="row">
-        <h1>Qui suis-je ?</h1>
+<div id="second">
+    <div class="container">
+        <div class="row">
+            <h1>Qui suis-je ?</h1>
+        </div>
     </div>
 </div>
 
-<div class="container third">
-    <div class="row">
-        <h1>Portfolio</h1>
+<div id="third">
+    <div class="container">
+        <div class="row">
+            <h1>Portfolio</h1>
+        </div>
     </div>
 </div>
 
-<div class="container fourth">
-    <div class="row">
-        <h1>Compétences</h1>
+<div id="fourth">
+    <div class="container fourth">
+        <div class="row">
+            <h1>Compétences</h1>
+        </div>
     </div>
 </div>
 
-<div class="container fifth">
-    <div class="row">
-        <h1>Contact</h1>
+<div id="fifth">
+    <div class="container fifth">
+        <div class="row">
+            <h1>Contact</h1>
+        </div>
     </div>
 </div>
 
