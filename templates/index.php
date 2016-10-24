d4dd6002f90c318445abae39640992e90de3ffb1
Oxyrio <axelandayme@hotmail.fr>
2016-10-24 15:18:59 +0200

    Ajout pour que tu m'aides

--- templates/index.php
+++ templates/index.php
@@ -4,31 +4,31 @@ include('header.php');
 
 ?>
 
-<div class="container">
+<div class="container first">
     <div class="row">
         <h1>Section d'entrée</h1>
     </div>
 </div>
 
-<div class="container">
+<div class="container second">
     <div class="row">
         <h1>Qui suis-je ?</h1>
     </div>
 </div>
 
-<div class="container">
+<div class="container third">
     <div class="row">
         <h1>Portfolio</h1>
     </div>
 </div>
 
-<div class="container">
+<div class="container fourth">
     <div class="row">
         <h1>Compétences</h1>
     </div>
 </div>
 
-<div class="container">
+<div class="container fifth">
     <div class="row">
         <h1>Contact</h1>
     </div>
